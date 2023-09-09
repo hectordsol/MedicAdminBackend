66bd25b7aaab921f8ba36dea49801db4b8a0310c
hectordsol <hectordsol@gmail.com>
2023-09-09 16:18:16 -0300

    allow_methos *

--- app/main.py
+++ app/main.py
@@ -28,7 +28,7 @@ origins = [
 app.add_middleware(
           CORSMiddleware,
           allow_origins=["*"],
-          allow_methods=["GET","POST","PUT","DELETE"],
+          allow_methods=["*"],
           allow_headers=["*"],
           allow_credentials=True
 )
